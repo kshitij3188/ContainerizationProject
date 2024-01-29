e4558816d56f7968a756654ba032210cac8edb65
Pooja Mangal <poojamangal15@gmail.com>
2024-01-29 11:36:32 +0100

    changed frontend form

--- frontend/src/components/InputBucketList.js
+++ frontend/src/components/InputBucketList.js
@@ -1,4 +1,35 @@
 import React, {Component} from "react"
+import styled from 'styled-components';
+
+const StyledForm = styled.form`
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+`;
+
+const StyledInput = styled.input`
+    padding: 10px;
+    font-size: 1rem;
+    border: 2px solid #1a5dab;
+    border-radius: 5px;
+    margin-right: 10px;
+    flex-grow: 2;
+`;
+
+const StyledButton = styled.button`
+    padding: 10px 20px;
+    font-size: 1rem;
+    background-color: #1a5dab;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+
+    &:hover {
+        background-color: #0e4377;
+    }
+`;
 
 class InputBucketList extends Component {
     state = {
@@ -25,11 +56,16 @@ class InputBucketList extends Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <input type="text" placeholder="Add item to your Bucket List.." value={this.state.title} name="title"
-                       onChange={this.onChange}/>
-                <button>ADD</button>
-            </form>
+            <StyledForm onSubmit={this.handleSubmit}>
+                <StyledInput 
+                    type="text"
+                    placeholder="Add item to your Bucket List.."
+                    value={this.state.title}
+                    name="title"
+                    onChange={this.onChange}
+                />
+                <StyledButton>ADD</StyledButton>
+            </StyledForm>
         )
     }
 }
