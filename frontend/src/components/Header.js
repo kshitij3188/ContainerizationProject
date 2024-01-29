6fcdd83b2f34d31a3b671f7abd156dc35ba329e7
Kshitij Kavimandan <kshitijk3188@gmail.com>
2024-01-29 11:24:29 +0100

    frontend header changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,33 @@
 import React from "react";
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+    background: #f4f7f6; /* Light grey background */
+    color: #333; /* Dark grey text */
+    text-align: center;
+    padding: 20px 0;
+`;
+
+const Title = styled.h1`
+    color: #1a5dab; /* Blue color for the title */
+    font-family: 'Arial', sans-serif;
+    font-size: 2.5rem;
+    margin: 0;
+`;
+
+const Subtitle = styled.p`
+    color: #1a5dab; /* Blue color for the subtitle */
+    font-family: 'Arial', sans-serif;
+    margin: 5px 0;
+`;
 
 const Header = () => {
     return (
-        <header>
-            <h1>My Bucket List Around Europe 🌍✈️</h1>
-            <p>Explore my travel goals and adventures across Europe.</p>
-            <p>Version 1.0 🗺️📸✨</p>
-        </header>
+        <StyledHeader>
+            <Title>My Bucket List Around Europe 🌍✈️</Title>
+            <Subtitle>Explore my travel goals and adventures across Europe.</Subtitle>
+            <Subtitle>Version 1.0 🗺️📸✨</Subtitle>
+        </StyledHeader>
     );
 };
 
--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,23 +1,20 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import React from "react";
+import React from "react"
 
 class ListItem extends React.Component {
     render() {
-        return (
-            <Card style={{ width: '18rem', marginBottom: '10px' }}>
-            <Card.Body>
-                <Card.Title>{this.props.todo.title}</Card.Title>
-                <input type="checkbox"
+        return <li>
+            <input
+                type="checkbox"
                 checked={this.props.todo.checked}
                 onChange={() => this.props.handleChangeProps(this.props.todo.id)}
-                /> Completed
-                <Button variant="danger" onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-                    Delete
-                </Button>
-            </Card.Body>
-            </Card>
-        )
+            />
+            <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+                Delete
+            </button>
+            {this.props.todo.title}
+
+            {/*<input type="checkbox" checked={this.props.todo.completed}/> {this.props.todo.title}*/}
+        </li>
     }
 }
 
