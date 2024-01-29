d55db553608a1a235f8581cf48fe2025e31ddb49
Pooja Mangal <poojamangal15@gmail.com>
2024-01-29 11:41:54 +0100

    listItem frontend modified

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,20 +1,22 @@
 import React from "react"
+import "./ListItem.css";
+
 
 class ListItem extends React.Component {
     render() {
-        return <li>
-            <input
-                type="checkbox"
-                checked={this.props.todo.checked}
-                onChange={() => this.props.handleChangeProps(this.props.todo.id)}
-            />
-            <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-                Delete
-            </button>
-            {this.props.todo.title}
-
-            {/*<input type="checkbox" checked={this.props.todo.completed}/> {this.props.todo.title}*/}
-        </li>
+        return (
+            <li className="list-item">
+                <input
+                    type="checkbox"
+                    checked={this.props.todo.checked}
+                    onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+                />
+                <span className="list-item-text">{this.props.todo.title}</span>
+                <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+                    Delete
+                </button>
+            </li>
+        );
     }
 }
 
