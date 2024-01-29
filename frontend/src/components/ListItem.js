c9dad96cf4f1792695928180080bdc7829acb1ca
Kshitij Kavimandan <kshitijk3188@gmail.com>
2024-01-29 11:06:51 +0100

    enhance frontend

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,20 +1,23 @@
-import React from "react"
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import React from "react";
 
 class ListItem extends React.Component {
     render() {
-        return <li>
-            <input
-                type="checkbox"
+        return (
+            <Card style={{ width: '18rem', marginBottom: '10px' }}>
+            <Card.Body>
+                <Card.Title>{this.props.todo.title}</Card.Title>
+                <input type="checkbox"
                 checked={this.props.todo.checked}
                 onChange={() => this.props.handleChangeProps(this.props.todo.id)}
-            />
-            <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-                Delete
-            </button>
-            {this.props.todo.title}
-
-            {/*<input type="checkbox" checked={this.props.todo.completed}/> {this.props.todo.title}*/}
-        </li>
+                /> Completed
+                <Button variant="danger" onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+                    Delete
+                </Button>
+            </Card.Body>
+            </Card>
+        )
     }
 }
 
